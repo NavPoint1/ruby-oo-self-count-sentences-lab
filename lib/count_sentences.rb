fad74613df03d9bd1de72d1dcd7506af02506d79
NavPoint1 <foleym156@gmail.com>
2020-08-29 15:28:37 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    period_split = self.split(". ").flatten
+    question_split = period_split.map { |fragment| fragment.split("? ")}.flatten
+    exclamation_split = question_split.map { |fragment| fragment.split("! ")}.flatten
+    binding.pry
+    return exclamation_split.size
   end
 end
